c4012409c4321be7062f5bad8847f6f58a3eda7d
Elshad001 <elshad.ismayilovv@mail.ru>
2023-02-14 22:46:25 +0400

    test4

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ sec1PrevBtn.addEventListener('click',()=>
     let scrollAmount=300;
     sec1Slider.scrollLeft-=scrollAmount;
 })
+
+
+const sec3Slider=document.querySelector('.section3-slider');
+const sec3NextBtn=document.querySelector('.sec3-next-btn');
+const sec3PrevBtn=document.querySelector('.sec3-prev-btn');
+
+sec3NextBtn.addEventListener('click',()=>
+{
+    let scrollAmount=300;
+    sec3Slider.scrollLeft+=scrollAmount;
+})
+
+sec3PrevBtn.addEventListener('click',()=>
+{
+    let scrollAmount=300;
+    sec3Slider.scrollLeft-=scrollAmount;
+})
